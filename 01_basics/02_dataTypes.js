08845b7e6db85a781ad65060c05edee0de7abd01
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-09-24 08:46:26 +0000

    datatype done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treate code as newer code standard
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+console.log(typeof null);
+console.log(typeof  undefined);
