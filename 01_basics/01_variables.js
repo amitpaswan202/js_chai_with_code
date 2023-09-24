6efc4d0bbe46a359a0cfc8764cb487ad71202205
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-09-24 08:14:15 +0000

    variable added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 2145;
+let accountName = "amit";
+let accountEmail = "amit@gmail.com"
+var accountPassword = "1235497";
+accountCity = "jaipure"
+// accountId = 25456
+
+accountName = "amit paswan"
+accountPassword = "123456789"
+accountCity= "banguluru"
+
+// console.log(accountId );\
+
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity])
