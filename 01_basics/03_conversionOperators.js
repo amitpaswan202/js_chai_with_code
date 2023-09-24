e3cdf4ccabcc99227dacbf985cfd03798eba7a45
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-09-24 11:42:14 +0000

    conversion and oprators 1

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,20 @@
+let name = "3"
+
+
+let nameInNumber = Number(name)
+console.log( nameInNumber);
+
+let nameInBoolean = Boolean(name)
+console.log( nameInBoolean);
+console.log(+name);
+
+// console.log(4 ** (1 / 2));
+// let apples = '2';
+
+// let oranges = '3';
+
+// // both values converted to numbers before the binary plus
+// console.log(+apples + +oranges); // 5
+
+// // the longer variant
+// console.log(Number(apples) + Number(oranges)); // 5
