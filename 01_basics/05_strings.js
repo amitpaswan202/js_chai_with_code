3d7b2b44370e9c039d81b6b82723a205cba973f7
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-09-30 12:35:38 +0000

    string praccgice still going

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -21,13 +21,33 @@ let name = "amit"
 // console.log(name.charAt(2));
 // console.log(name.at(2));
 
+// ===================
+// let a = "a";
+// let b = "b";
+// if(a<b){
+//     console.log(`${a} is less than ${b}`);
+// }else if(a>b){
+//     console.log(`${a} is geather than ${b}`);
+// }else{
+// console.log(`${a} is equal to ${b}`);
+// }
 
-let a = "a";
-let b = "b";
-if(a<b){
-    console.log(`${a} is less than ${b}`);
-}else if(a>b){
-    console.log(`${a} is geather than ${b}`);
-}else{
-console.log(`${a} is equal to ${b}`);
-}
+// ============================
+const contentString = "The quick brown fox jumps 24 over the lazy dog. If the dog barked, was it really lazy?";
+
+// document.body.innerHTML = contentString.big()
+// contentString.
+// const view =document.body.innerHTML = ram.big()
+// console.log(contentString.charCodeAt(3));
+// console.log(contentString.codePointAt(3));
+// console.log(contentString.concat("raajesh"));
+// console.log(contentString.endsWith('world'));
+// console.log(contentString.startsWith('Hello'))
+// console.log(contentString.includes('Hello'));
+// console.log(contentString.indexOf('dog'));
+
+// console.log(contentString.lastIndexOf('dog'));
+// console.log(contentString.length);
+// console.log(contentString.match(/[A-Z]/g));
+// console.log(contentString.match(/[A-Z]/gi));
+console.log(contentString.match(/(?<animal>dog|cat) jumps over/));
