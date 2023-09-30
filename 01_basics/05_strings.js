7ba7696c81c7915a02ede0b80d465de728c4a390
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-09-30 10:17:51 +0000

    this is proctice string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const newName = "hello amit paswan"
+// console.log(newName.endsWith('paswan'));
+// const elem = document.createElement("a");
+// elem.innerHTML = newName
+// document.body.appendChild(elem)
+// console.log(newName.anchor('paswan'));
+// console.log(newName)
+
+
+// console.log(`hello ${upi}`);
+// =========================================================
+
+// const contentString = "Hello, world";
+// const elem = document.createElement("a");
+// elem.innerText = contentString;
+// document.body.appendChild(elem);
+
+// ================================practice=========================
+let name = "amit"
+// console.log(name[3]);
+// console.log(name.charAt(2));
+// console.log(name.at(2));
+
+
+let a = "a";
+let b = "b";
+if(a<b){
+    console.log(`${a} is less than ${b}`);
+}else if(a>b){
+    console.log(`${a} is geather than ${b}`);
+}else{
+console.log(`${a} is equal to ${b}`);
+}
