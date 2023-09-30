6f1251f26738aa366c76a19e99c361b2d17e2e3f
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-09-30 14:31:09 +0000

    this is date commit

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,8 @@
+const myDate = new Date()
+console.log(myDate);
+console.log(myDate.toJSON());
+console.log(myDate.toString());//Sat Sep 30 2023 14:16:40 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString())//Sat Sep 30 2023
+console.log(myDate.toLocaleDateString())//   20/09/2023
+console.log(myDate.toLocaleString())//  9/30/2023, 2:18:14 PM
+console.log(typeof myDate); /////////////////////// type of date in object
