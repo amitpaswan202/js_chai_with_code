fa39e737b6574ae1e7c4c7c0962fee7c6f2f83f7
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-10-02 07:58:44 +0000

    array decclair done

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,1 +1,6 @@
-console.log("hello ");
+// ++++++++++++++++ array declairation ++++++++++++++++++++++
+const myArray = [25,35,64,25,69,]
+const myHero = ["saktiman","nagraj"]
+const myArray2 = new Array("hello",25,"for",85)
+// console.log(myArray[2]);
+// console.log(myArray2);
