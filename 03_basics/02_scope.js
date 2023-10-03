66c43ff70dc0ae81b038260e8988e965e1d8b893
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-10-03 18:34:09 +0000

    scope intersection

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -12,3 +12,33 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
