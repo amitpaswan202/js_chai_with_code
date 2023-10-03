dc900a2e1dc889ed72eeacface0568471445ee6e
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-10-03 18:38:40 +0000

    function and expression is clear

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -42,3 +42,13 @@ if (true) {
 
 
 // ++++++++++++++++++ interesting ++++++++++++++++++
+console.log(addone(5))
+function addone(num1){
+    return num1 + 1
+}
+
+
+console.log(addtwo(6));
+const addtwo = function(num2){
+    return num2 + 2
+}
