1a510f7f22efcada28f1b91f1d893cbeacaedb60
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-10-04 09:21:50 +0000

    if else and switch case completd

---
+++ 04_controll_flow/switch.js
@@ -0,0 +1,19 @@
+let month  = 2;
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("fab");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("month name is not present");
+        break;
+}
