79adcf412dd23291c5693b952af100deaf127cb2
Amit Paswan <54396523+amitpaswan202@users.noreply.github.com>
2023-10-07 07:17:19 +0000

    promises cath cand finaly excuted

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -9,18 +9,45 @@
 // })
 
 
-new Promise(function(resolve,rejest){
-    setTimeout(function(){
-        console.log(' asynk promise two is called')
-        resolve({username: "Chai", email: "chai@example.com"})
-    },1000)
-}).then(function(){
-    console.log('promise two consumed')
-})
+// new Promise(function(resolve,rejest){
+//     setTimeout(function(){
+//         console.log(' asynk promise two is called')
+//         resolve({username: "Chai", email: "chai@example.com"})
+//     },1000)
+// }).then(function(){
+//     console.log('promise two consumed')
+// })
 
 
-const promiseThree = new Promise(function(resolve, reject){
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username: "Chai", email: "chai@example.com"})
+//     }, 1000)
+// })
+// promiseThree.then(function(user){
+//     console.log(user)
+// })
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    const userLogged = false
     setTimeout(function(){
+       if(userLogged === true){
         resolve({username: "Chai", email: "chai@example.com"})
+       }
+       else{
+        console.log("something went wrong");
+       }
+      
     }, 1000)
+    
+})
+promiseFour.then(function(user){
+     return user.username
+}).then((e) =>{
+    console.log(e)
+}).catch(function(err){
+    console.log(err)
+}).finally(function(){
+    console.log();
 })
